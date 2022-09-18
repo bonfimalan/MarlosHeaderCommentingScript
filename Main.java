731f9b936d4ef08dc8647d0e5c80f695f86f4788
bonfimalan <alanbonfim2000@gmail.com>
2022-09-18 16:00:58 -0300

    Writing header in Java files

--- Main.java
+++ Main.java
@@ -1,17 +1,26 @@
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.nio.file.attribute.FileTime;
+import java.text.SimpleDateFormat;
+import java.util.Scanner;
 
 public class Main {
   private static final String JAVA_FILE_EXTENSION = ".java";
+  private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yy HH:mm");
+
   public static void main(String[] args) {
-    if (args.length == 0) 
+    if (args.length == 0)
       printAndExitProgram("No directory passed");// the path was not passed
 
     File mainFolder = new File(args[0]);
-    
-    if(mainFolder.isFile()) 
+
+    if (mainFolder.isFile())
       printAndExitProgram("You must pass a directory");
 
-    if(!mainFolder.exists()) 
+    if (!mainFolder.exists())
       printAndExitProgram("Directory don't exists");
 
     commentJavaFiles(mainFolder);
@@ -23,18 +32,84 @@ public class Main {
   }
 
   public static void commentJavaFiles(File f) {
-    if(f.isFile() && f.getName().endsWith(JAVA_FILE_EXTENSION))
+    if (f.isFile() && f.getName().endsWith(JAVA_FILE_EXTENSION))
       addHeaderComment(f);
 
     // so it's a directory
     File[] files = f.listFiles();
-    if(files == null) return;
+    if (files == null)
+      return;
 
-    for(File file : files)
+    for (File file : files)
       commentJavaFiles(file);
   }
 
   public static void addHeaderComment(File f) {
-    
+    try {
+      BasicFileAttributes fileAtt;
+
+      fileAtt = Files.readAttributes(
+          f.toPath(), BasicFileAttributes.class);
+
+      String header = headerComment(
+          f.getName(),
+          formatDate(fileAtt.creationTime()),
+          formatDate(f.lastModified()));
+
+      writeNewFile(header, f);
+
+    } catch (IOException e) {
+    }
+  }
+
+  public static void writeNewFile(String header, File oldFile) {
+    try {
+      Scanner scanner = new Scanner(oldFile);
+
+      String firstLine = scanner.nextLine();
+      if(hasHeaderComment(firstLine)) {
+        scanner.close();
+        return;
+      }
+
+      System.out.println("Adding header to " + oldFile.getName());
+
+      File newFile = new File(oldFile.getAbsolutePath());
+      FileWriter fileWriter = new FileWriter(newFile);
+      fileWriter.write(header);
+      fileWriter.write(firstLine + "\n");
+      
+
+      while (scanner.hasNextLine()) {
+        fileWriter.write(scanner.nextLine() + "\n");
+      }
+      
+      fileWriter.close();
+      scanner.close();
+    } catch (Exception e) {
+    }
+  }
+
+  public static String formatDate(FileTime fileTime) {
+    return DATE_FORMAT.format(fileTime.toMillis());
+  }
+
+  public static String formatDate(long time) {
+    return DATE_FORMAT.format(time);
+  }
+
+  public static boolean hasHeaderComment(String firstLine) {
+    return firstLine.startsWith("/***************************");
+  }
+
+  public static String headerComment(String fileName, String creationDate, String lastModified) {
+    return "/********************************************************************" + "\n" +
+        "* Author: Alan Bonfim Santos" + "\n" +
+        "* Registration: 201911912" + "\n" +
+        "* Initial date: " + creationDate + "\n" +
+        "* Last update: " + lastModified + "\n" +
+        "* Name: " + fileName + "\n" +
+        "* Function:" + "\n" +
+        "*******************************************************************/" + "\n";
   }
 }
